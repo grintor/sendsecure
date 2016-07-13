3111cb51c3541ffa7ad7c1055fbe5b186f9d9143
Chris Wheeler <grintor@gmail.com>
2016-07-13 13:39:12 -0400

    restructure for credentials
    restructured so that the validated credentials could be passed to php along with the message

--- smtp.py
+++ smtp.py
@@ -2,37 +2,48 @@ from secure_smtpd import SMTPServer
 from email import Parser
 from requests import post
 from json import dumps
-
-class CredentialValidator(object):
-	def validate(self, username, password):
-		r = post("https://www.sendsecure.org/smtp_auth.php", data = {'username':username, 'password':password})
-		if r.status_code == 202: return True
-		return False
-
-class SSLSMTPServer(SMTPServer):
-	def process_message(self, peer, mailfrom, rcpttos, message_data):
-
-		# parse the email into an object
-		message_data = Parser.Parser().parsestr(message_data)
 		
-		# check if the message is multipart (has attachments)
-		is_multipart = message_data.is_multipart()
+class authSMTP():
 
-		# convert the object to json
-		message_data = dumps(message_data, default=lambda o: o.__dict__, ensure_ascii=False)
+	def __init__(self):
+		self.username = None
+		self.password = None
 
-		post("https://www.sendsecure.org/test.php", data = {	'peer':peer,
-																			'mailfrom':mailfrom,
-																			'rcpttos':rcpttos,
-																			'is_multipart':is_multipart,
-																			'message_data':message_data})
+	class credentialManager(object):
+		def validate(self, username, password):
+			r = post("https://www.sendsecure.org/smtp_auth.php", data = {'username':username, 'password':password})
+			if r.status_code == 202:
+				authSMTP.username = username
+				authSMTP.password = password
+				return True
+			return False
 
-server = SSLSMTPServer(
+	class server(SMTPServer):
+		def process_message(self, peer, mailfrom, rcpttos, message_data):
+
+			# parse the email into an object
+			message_data = Parser.Parser().parsestr(message_data)
+			
+			# check if the message is multipart (has attachments)
+			is_multipart = message_data.is_multipart()
+
+			# in-place convert the message_data object to json
+			message_data = dumps(message_data, default=lambda o: o.__dict__, ensure_ascii=False)
+
+			post("https://www.sendsecure.org/smtp_post.php", data = {	'peer':peer,
+																		'mailfrom':mailfrom,
+																		'rcpttos':rcpttos,
+																		'username':authSMTP.username,
+																		'password':authSMTP.password,
+																		'is_multipart':is_multipart,
+																		'message_data':message_data})
+
+authSmtpServer = authSMTP.server(
 	('127.0.0.1', 2525),
 	None,
 	require_authentication=True,
 	ssl=False,
-	credential_validator=CredentialValidator(),
+	credential_validator=authSMTP.credentialManager(),
 	)
 
-server.run()
+authSmtpServer.run()
