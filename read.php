fd1223ffbb65da5620956528a7dce2f799c4c3fa
Chris Wheeler <grintor@gmail.com>
2016-08-05 18:37:44 -0400

    Update read.php

--- read.php
+++ read.php
@@ -1,22 +1,34 @@
 <?php
 header("content-type: text/html; charset=UTF-8");
 require_once('./sqliConnect.php');
+require_once('./functions.php');
 require_once('./htmlpurifier-4.8.0-standalone/HTMLPurifier.standalone.php');
 $HTMLPurifier = new HTMLPurifier();
 require_once('./smarty-3.1.29/Smarty.class.php');
 $smarty = new Smarty;
 
-
-// $sqlRequest = sprintf(
-		// "SELECT * FROM emails WHERE uniqid = '%s'",
-		// mysqli_real_escape_string($mysqli, "579a432c798633.60071576")
-// );
-
-$sqlRequest = "SELECT * FROM emails ORDER BY timestamp DESC LIMIT 1";
+$sqlRequest = sprintf("SELECT
+	uniqid,
+	_to,
+	_from,
+	_datetime,
+	_subject,
+	_replyto,
+	_cc,
+	UNCOMPRESS(AES_DECRYPT(_parts, UNHEX('%s'), '%s')) AS _parts,
+	UNCOMPRESS(AES_DECRYPT(_attachments, UNHEX('%s'), '%s')) AS _attachments
+	FROM emails WHERE uniqid = '%s'",
+	mysqli_real_escape_string($mysqli, $_GET['key']),
+	mysqli_real_escape_string($mysqli, $_GET['id']),
+	mysqli_real_escape_string($mysqli, $_GET['key']),
+	mysqli_real_escape_string($mysqli, $_GET['id']),
+	mysqli_real_escape_string($mysqli, $_GET['id'])
+);
 
 if (!$sqlResult = $mysqli->query($sqlRequest)) {
-    echo 'DB query failed';
-	echo $mysqli->error;
+    print 'DB query failed';
+	print ': ' . $mysqli->error;
+	print "\nquery: " . $sqlRequest;
     exit;
 }
 
@@ -24,45 +36,43 @@ $sqlResult = mysqli_fetch_array($sqlResult, MYSQL_ASSOC);
 
 $id = $sqlResult['uniqid'];
 
-$parts = json_decode($sqlResult['parts'], true);
-if(isset($parts[1])){
+$parts = json_decode($sqlResult['_parts'], true);
+if(isset($parts[1])){ // part[1] means we have html
 	$message = $parts[1]['content'];
 	$message = $HTMLPurifier->purify($message);
-} elseif (isset($parts[0])) {
+} elseif (isset($parts[0])) { // if we don't have a part[1] then we only have a text message in part[0]
 	$message = '<pre>' . htmlspecialchars($parts[0]['content']) . '</pre>';
 	$message = $HTMLPurifier->purify($message);
 } else {
 	$message = "NO MSG";
 }
 
-$to = null;
-foreach(json_decode($sqlResult['to'], true) as $t) {
-	if(!$to){
-		$to = '&quot;' . $t['name'] . '&quot;' . ' &lt;' . $t['email'] . '&gt;';
-	} else {
-		$to .= ',<br />' . '&quot;' . $t['name'] . '&quot;' . ' &lt;' . $t['email'] . '&gt;';
-	}
+$from = addressListHTML(json_decode($sqlResult['_from'], true));
+$to = addressListHTML(json_decode($sqlResult['_to'], true));
+
+$cc = json_decode($sqlResult['_cc'], true);
+if ($cc){
+	$cc = addressListHTML(json_decode($sqlResult['_cc'], true));
+	$cc = "<p><span class = 'vars'>CC:</span><span>" . $cc . "</span></p>";
+} else {
+	$cc = null;	// if $cc is an empty array, make it null (smarty hates empty arrays)
 }
 
-$from = null;
-foreach(json_decode($sqlResult['from'], true) as $f) {
-	// the part that is used in each instance of the list
-	$x = '&quot;' . $f['name'] . '&quot;' . ' &lt;' . $f['email'] . '&gt;';
-	if(!$from){
-		// the part that is only used for the first element of the list
-		$from = $x;
-	} else {
-		// the part that is used for each subsequent element of the list
-		$from .= ',<br />' . $x;
-	}
+$replyTo = json_decode($sqlResult['_replyto'], true);
+if ($replyTo){
+	$replyTo = addressListHTML(json_decode($sqlResult['_replyto'], true));
+	$replyTo = "<p><span class = 'vars'>Reply To:</span><span>" . $replyTo . "</span></p>";
+} else {
+	$replyTo = null; // if $replyTo is an empty array, make it null (smarty hates empty arrays)
 }
 
-$attachmentArr = json_decode($sqlResult['attachments'], true);
+
+$attachmentArr = json_decode($sqlResult['_attachments'], true);
 $attachments = null;
 $index = 0;
 foreach($attachmentArr as $f) {
 	// the part that is used in each instance of the list
-	$x = '<a href="download.php?id='. $id .'&index=' . $index . '">' . $f['filename'] . '</a>';
+	$x = '<a href="download.php?id='. $id .'&index=' . $index . '&key=' . $_GET['key'] . '">' . $f['filename'] . '</a>';
 	$index++;
 	if(!$attachments){
 		// the part that is only used for the first element of the list
@@ -75,17 +85,22 @@ foreach($attachmentArr as $f) {
 // the part that is appended at the end of the list
 if($attachments) $attachments .= '</span></p>';
 
-$date = $sqlResult['datetime'];
+$date = $sqlResult['_datetime'];
 $date = strtotime($date);
 $date = date('l, F jS, Y \a\t h:i:s A', $date);
 
-$subject = $sqlResult['subject'];
+$subject = $sqlResult['_subject'];
 
+$smarty->assign('id', $_GET['id']);
+$smarty->assign('index', $_GET['index']);
+$smarty->assign('key', $_GET['key']);
 $smarty->assign('subject', $subject);
 $smarty->assign('attachments', $attachments);
 $smarty->assign('from', $from);
 $smarty->assign('date', $date);
 $smarty->assign('to', $to);
+$smarty->assign('replyTo', $replyTo);
+$smarty->assign('cc', $cc);
 $smarty->assign('message', $message);
 $smarty->display('read.tpl');
 
