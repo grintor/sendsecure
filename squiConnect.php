ad101e8ef889955327194be8db439e06fd9c0e81
Chris Wheeler <grintor@gmail.com>
2016-08-05 18:36:22 -0400

    Update squiConnect.php

--- squiConnect.php
+++ squiConnect.php
@@ -1,8 +1,13 @@
 <?php
 
-$mysqli = new mysqli('127.0.0.1', 'blah', 'blah', 'blah');
+$mysqli = new mysqli('127.0.0.1', 'sendsecure', '4rfvgy7u', 'sendsecure');
 if ($mysqli->connect_errno) {
     echo 'could not contact DB';
+    exit;
+}
+
+if (!$sqlResult = $mysqli->query('SET @@session.block_encryption_mode = "aes-256-cbc"')) {
+    echo 'Failed to initilize encryption';
     exit;
 }
 
