7937794d1d20563dc531f34969293221e8368b75
Chris Wheeler <grintor@gmail.com>
2016-08-05 18:37:14 -0400

    Update download.php

--- download.php
+++ download.php
@@ -2,19 +2,24 @@
 require_once('./sqliConnect.php');
 
 $sqlRequest = sprintf(
-		"SELECT * FROM emails WHERE uniqid = '%s'",
+		"SELECT UNCOMPRESS(AES_DECRYPT(_attachments, UNHEX('%s'), '%s')) AS _attachments FROM emails WHERE uniqid = '%s'",
+		mysqli_real_escape_string($mysqli, $_GET['key']),
+		mysqli_real_escape_string($mysqli, $_GET['id']),
 		mysqli_real_escape_string($mysqli, $_GET['id'])
 );
 
+
+
 if (!$sqlResult = $mysqli->query($sqlRequest)) {
-    echo 'DB query failed';
-	echo $mysqli->error;
+    print 'DB query failed';
+	print ': ' . $mysqli->error;
+	print "\nquery: " . $sqlRequest;
     exit;
 }
 
 $sqlResult = mysqli_fetch_array($sqlResult, MYSQL_ASSOC);
 
-$attachments = json_decode($sqlResult['attachments'], true);
+$attachments = json_decode($sqlResult['_attachments'], true);
 
 $attachment = base64_decode($attachments[$_GET['index']]['content']);
 
